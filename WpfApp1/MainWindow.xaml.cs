9213fac06dfa187f731391eb220792b3dc410c1c
Elio <elio.rbg@gmail.com>
2025-02-27 15:37:47 -0500

    Cleanup

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,40 +1,27 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WpfApp1.Types;
+﻿using WpfApp1.Types;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            var assemblyPath = @"path\to\MessageDefinitions.dll";
+            string assemblyPath = @"path\to\MessageDefinitions.dll";
             var vmTypes = MessageFactory.CreateMessageViewModelTypes(assemblyPath);
             MessageFactory.RegisterDataTemplates(this, vmTypes);
 
-            // Test with some instances
+            // Create instances for testing
             var instances = new List<object>();
             foreach (var vmType in vmTypes.Values)
             {
-                var instance = Activator.CreateInstance(vmType);
+                object instance = Activator.CreateInstance(vmType);
                 if (instance != null)
                 {
-                    // Optionally set Content for testing
                     var contentProp = vmType.BaseType.GetProperty("Content");
-                    var contentInstance = Activator.CreateInstance(vmType.BaseType.GetGenericArguments()[0]);
+                    var contentType = vmType.BaseType.GetGenericArguments()[0];
+                    var contentInstance = Activator.CreateInstance(contentType);
                     contentProp.SetValue(instance, contentInstance);
                     instances.Add(instance);
                 }
--- WpfApp1/Types/MessageFactory.cs
+++ WpfApp1/Types/MessageFactory.cs
@@ -9,6 +9,15 @@ using System.Windows.Markup;
 namespace WpfApp1.Types
 {
 
+    using System;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Markup;
+    using System.Windows.Controls;
+
     public class MessageFactory
     {
         private static readonly string AssemblyName = "DynamicMessages";
@@ -17,40 +26,41 @@ namespace WpfApp1.Types
 
         public static Dictionary<string, Type> CreateMessageViewModelTypes(string assemblyPath)
         {
-            var assembly = Assembly.LoadFrom(assemblyPath);
+            // Load the external assembly
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
             var messageTypes = assembly.GetTypes()
                 .Where(t => t.IsClass &&
                            t.Namespace == "lib.messages.definition" &&
                            !t.IsAbstract)
                 .ToList();
 
-            var asmName = new AssemblyName(AssemblyName);
-            asmName.Version = new Version(1, 0, 0, 0); // Optional: Versioning for clarity
-            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.Run);
-            var moduleBuilder = assemblyBuilder.DefineDynamicModule(ModuleName);
+            // Define a dynamic assembly (in-memory only)
+            AssemblyName asmName = new AssemblyName(AssemblyName);
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(ModuleName);
 
             var createdTypes = new Dictionary<string, Type>();
 
             foreach (var messageType in messageTypes)
             {
-                var newTypeName = $"{AppNamespace}.{messageType.Name}Vm";
-                var typeBuilder = moduleBuilder.DefineType(
+                string newTypeName = $"{AppNamespace}.{messageType.Name}Vm";
+                TypeBuilder typeBuilder = moduleBuilder.DefineType(
                     newTypeName,
                     TypeAttributes.Public | TypeAttributes.Class,
                     typeof(Message<>).MakeGenericType(messageType)
                 );
 
-                var ctorBuilder = typeBuilder.DefineConstructor(
+                ConstructorBuilder ctorBuilder = typeBuilder.DefineConstructor(
                     MethodAttributes.Public,
                     CallingConventions.Standard,
                     Type.EmptyTypes
                 );
-                var ilGen = ctorBuilder.GetILGenerator();
+                ILGenerator ilGen = ctorBuilder.GetILGenerator();
                 ilGen.Emit(OpCodes.Ldarg_0);
                 ilGen.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
                 ilGen.Emit(OpCodes.Ret);
 
-                var createdType = typeBuilder.CreateType();
+                Type createdType = typeBuilder.CreateType();
                 createdTypes[messageType.Name] = createdType;
             }
 
@@ -59,13 +69,13 @@ namespace WpfApp1.Types
 
         public static void RegisterDataTemplates(Window window, Dictionary<string, Type> vmTypes)
         {
-            var resources = new ResourceDictionary();
+            ResourceDictionary resources = new ResourceDictionary();
 
             foreach (var kvp in vmTypes)
             {
-                var vmType = kvp.Value;
-                var typeName = vmType.Name; // e.g., TextMessageVm
-                var xaml = $@"
+                Type vmType = kvp.Value;
+                string typeName = vmType.Name; // e.g., TextMessageVm
+                string xaml = $@"
                 <DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
                               xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
                               DataType='{{x:Type local:{typeName}}}'>
@@ -74,12 +84,12 @@ namespace WpfApp1.Types
 
                 try
                 {
-                    // Parse the XAML, providing the dynamic assembly context
-                    var template = (DataTemplate)XamlReader.Parse(xaml, new ParserContext
+                    var parserContext = new ParserContext
                     {
                         XmlnsDictionary = { { "local", AppNamespace } },
                         XamlTypeMapper = new XamlTypeMapper(new[] { vmType.Assembly.FullName })
-                    });
+                    };
+                    var template = (DataTemplate)XamlReader.Parse(xaml, parserContext);
                     resources.Add(new DataTemplateKey(vmType), template);
                 }
                 catch (Exception ex)
