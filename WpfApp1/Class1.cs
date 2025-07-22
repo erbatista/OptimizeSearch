c9c4f5efcaebbf643ba3d096ffce677f2e38959d
Elio <elio.rbg@gmail.com>
2025-07-22 15:37:54 -0400

    Cleanup

--- WpfApp1/Class1.cs
+++ WpfApp1/Class1.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfApp1
 {
-    class Class1
+    public class MyViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<MyItem> _fullCollection;
+        public ObservableCollection<MyItem> FullCollection
+        {
+            get => _fullCollection;
+            set
+            {
+                _fullCollection = value;
+                OnPropertyChanged(nameof(FullCollection));
+                OnPropertyChanged(nameof(FirstThreeItems)); // Update derived property
+            }
+        }
+
+        // Property exposing only the first three items
+        public IEnumerable<MyItem> FirstThreeItems => FullCollection?.Take(3);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
