629adeaa8e6cd88b28dd7128893e63341e568bd6
Elio <elio.rbg@gmail.com>
2025-07-22 15:30:03 -0400

    Clweanup

---
+++ WpfApp1/TakeFirstThreeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp1;
+
+public class TakeFirstThreeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IEnumerable<object> collection)
+        {
+            return collection.Take(3).ToList();
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
